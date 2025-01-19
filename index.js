9be7c95e1b21c93bbe95b5b820691756e19fb9ba
rzhou10 <rzhou10@gmail.com>
2025-01-19 17:53:19 -0500

    Added YouTube command

--- index.js
+++ index.js
@@ -29,20 +29,17 @@ client.on("messageCreate", async function (message) {
     const nationalDexId = args[0];
 
     const pokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${nationalDexId}/`);
+
     if (pokemon.data.name === args[1]) {
       channel.send('Congrats, you guessed correctly!');
     } else {
       channel.send(`Sorry, the correct Pokemon is ${pokemon.data.name.charAt(0).toUpperCase() + pokemon.data.name.slice(1)}`);
     }
-  }
-
-  if (command === "weather") {
+  } else if (command === "weather") {
     const weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${config.WEATHER_API_KEY}q=${config.WEATHER_LOCATION}&aqi=no`);
 
     channel.send(`It is currently ${weather.data.current.temp_f} in ${config.WEATHER_LOCATION}`);
-  }
-
-  if (command === "metalarchives") {
+  } else if (command === "metalarchives") {
     const bandName = args[0];
     const band = (await axios.get(`https://metal-api.dev/search/bands/name/${bandName}`)).data;
     channel.send("Searching bands....");
@@ -53,13 +50,22 @@ client.on("messageCreate", async function (message) {
     // TODO: Refactor so that the message can be filtered and paginated
     while (message.length < 1800) {
 
-       message += `\n${band[index].name} (${band[index].country})\n[Metal Archives](${band[index].link})`;
-       index++;
+      message += `\n${band[index].name} (${band[index].country})\n[Metal Archives](${band[index].link})`;
+      index++;
     }
 
     channel.send(message);
-  }
+  } else if (command === "youtube") {
+    const videos = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCjYac0sM1ciPFIKNmW4pF1Q&maxResults=10&order=date&type=video&key=${config.YOUTUBE_KEY}`);
 
-});
+    if (videos.data.items.length > 0) {
+      const vids = videos.data.items.slice(2);
+      channel.send(`Heavy Metallurgy recent videos: [${vids[0].snippet.title}](https://www.youtube.com/watch?v=VGzkVWX5Ufs${vids[0].id.videoId}) and [${vids[1].snippet.title}](https://www.youtube.com/watch?v=VGzkVWX5Ufs${vids[1].id.videoId})`);
+    } else {
+      channel.send(`No new videos from Heavy Metallurgy!`);
+    }
+  }
+}
+);
 
 console.log("General bot finished loading!")
